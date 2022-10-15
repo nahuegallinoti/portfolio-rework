9cde2ea4b3aa99ecba778337bff0ed53b1cc88b7
Nahuel Gallinoti <nahuel.gallinoti@incluit.com>
2022-10-15 20:22:03 -0300

    add api method

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import LayoutSection from "../Shared/LayoutSection";
 
 const Contact = () => {
+  //declaramos el email como un state porque queremos controlar el input
+  //los demas valores se mapean con el atributo name del input
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    //hay que setear a mano los input controlados
+    formData.set("email", email);
+
+    //transforma elementos del form en objeto json
+    const objectData = Object.fromEntries(formData);
+
+    fetch("http://localhost:3000/api/contact/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(objectData),
+    })
+      .then(async (res) => {
+        console.log(await res.json());
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <LayoutSection
       title="Contact"
@@ -11,7 +39,7 @@ const Contact = () => {
     >
       <article className="flex flex-col my-10 items-center">
         <div>
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <div>
               <label
                 htmlFor="firstName"
@@ -55,11 +83,11 @@ const Contact = () => {
               </label>
               <div className="mt-1 flex flex-col items-center">
                 <input
-                  type="text"
-                  name="email"
+                  type="email"
                   id="email"
                   className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md h-8 w-72"
                   placeholder="adri.cervera@gmail.com"
+                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
             </div>
