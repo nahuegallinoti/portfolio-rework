29418fa8b5c8801dbdf6598dfea9454be5054259
Nahuel Gallinoti <nahuel.gallinoti@incluit.com>
2022-10-24 04:45:30 -0300

    fix package json

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-// module.exports = nextConfig
-
-const withPWA = require('next-pwa')
-
 const nextConfig = {
-  dest: 'public',
+  reactStrictMode: true,
+  swcMinify: true,
 }
-
-
-module.exports = withPWA(nextConfig)
+module.exports = nextConfig
