77ec6524a568fa36f4046db32717695a71689f60
Nahuel Gallinoti <nahuel.gallinoti@incluit.com>
2023-01-13 06:41:06 +0100

    add output project

--- data/projects.ts
+++ data/projects.ts
@@ -2,12 +2,13 @@ import { IProject } from "../interfaces/IProject";
 
 export const projects: IProject[] = [
   {
-    title: "NFT Marketplace",
+    
+    title: "Output Club",
     description:
-      "Marketplace to mint, buy, sell and stake NFT's using custom token (LUNGO Token)",
-    tags: ["Angular 13", "Truffle", "Ganache", "Solidity"],
-    source: "https://github.com/nahuegallinoti/lungo-token-nft",
-    visit: "https://google.com",
+      "Landing page for a audiovisual production company",
+    tags: ["Next 13", "Tailwind"],
+    source: "https://github.com/nahuegallinoti/output-club",
+    visit: "https://output-club-f923m7z6d-nahuegallinoti.vercel.app/",
     id: 0,
   },
   {
@@ -29,28 +30,21 @@ export const projects: IProject[] = [
     id: 2,
   },
   {
+    title: "NFT Marketplace",
+    description:
+      "Marketplace to mint, buy, sell and stake NFT's using custom token (LUNGO Token)",
+    tags: ["Angular 13", "Truffle", "Ganache", "Solidity"],
+    source: "https://github.com/nahuegallinoti/lungo-token-nft",
+    visit: "https://google.com",
+    id: 3,
+  },
+  {
     title: "Giphy App",
     description: "Web application to search GIFs",
     tags: ["React"],
     source: "https://github.com/nahuegallinoti/giphy-react-app",
     visit: "https://giphy-app-react-hooks.herokuapp.com/",
-    id: 3,
-  },
-  {
-    title: "ChatApp",
-    description: "Chat app",
-    tags: ["React", "Socket.io", "Node.js"],
-    source: "https://github.com/nahuegallinoti/chatApp",
-    visit: "https://react-socketio-express-chat.herokuapp.com/",
     id: 4,
-  },
-  {
-    title: "Posts App",
-    description: "App to make posts",
-    tags: ["React", "Node.js", "Express", "Tailwind"],
-    source: "https://github.com/nahuegallinoti/MERN-stack-posts",
-    visit: "https://mern-stack-nagu.herokuapp.com/",
-    id: 5,
   },
   {
     title: "Portfolio",
@@ -58,6 +52,22 @@ export const projects: IProject[] = [
     tags: ["Next.js", "Tailwind"],
     source: "https://github.com/nahuegallinoti/portfolio-rework",
     visit: "https://portfolio-rework-rouge.vercel.app/",
+    id: 5,
+  },
+  {
+    title: "Posts App",
+    description: "App to make posts",
+    tags: ["React", "Node.js", "Express", "Tailwind"],
+    source: "https://github.com/nahuegallinoti/MERN-stack-posts",
+    visit: "https://mern-stack-nagu.herokuapp.com/",
     id: 6,
+  },
+  {
+    title: "ChatApp",
+    description: "Chat app",
+    tags: ["React", "Socket.io", "Node.js"],
+    source: "https://github.com/nahuegallinoti/chatApp",
+    visit: "https://react-socketio-express-chat.herokuapp.com/",
+    id: 7,
   },
 ];
